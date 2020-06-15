86f9935692bf015212180cc72eb386865cf2bc65
Vineet Ranjan <vineetranjan65@gmail.com>
2020-06-15 23:25:55 +0530

    Update timetable.py

--- timetable.py
+++ timetable.py
@@ -2,99 +2,72 @@ import requests
 import json
 import datetime
 
-day = datetime.datetime.today().weekday()
-days_list = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-today_day = days_list[day]
-print(today_day)
 
+def lambda_handler(event=None, context=None):
+    day = datetime.datetime.today().weekday()
+    days_list = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    today_day = days_list[day]
 
-Monday = ["Artificial Intelligence,", "Power Electronics,", "Signals & Systems,", "Microcontrollers,", "Placement Training"]
-Tuesday = ["Signals & Systems,", "Artificial Intelligence,", "Power Electronics,", "Management & Enterpreneurship,", "PE Lab"]
-Wednesday = ["Power Electronics,", "Management & Enterpreneurship,", "Microcontrollers Lab"]
-Thursday = ["Estimation & Costing,", "Microcontrollers,", "Artificial Intelligence,", "Signals & Systems"]
-Friday = ["Power Electronics,", "Microcontrollers,", "Management & Enterpreneurship,", "Estimation & Costing"]
-Saturday = ["Estimation & Costing,", "Signals & Systems,", "Microcontrollers,", "Management & Enterpreneurship"]
+    Monday = ["Subject 1,", "Subject 2,", "Subject 3"]
+    Tuesday = ["Subject 1,", "Subject 2,", "Subject 3"]
+    Wednesday = ["Subject 1,", "Subject 2,", "Subject 3"]
+    Thursday = ["Subject 1,", "Subject 2,", "Subject 3"]
+    Friday = ["Subject 1,", "Subject 2,", "Subject 3"]
+    Saturday = ["Subject 1,", "Subject 2,", "Subject 3"]
 
-
-wekbook_url = '-------------------URL------------------'
-
-send_data = "Good Morning Vineet!! \nToday it's " + days_list[day] + "\n\nTimetable is :"
-
-def webhook_send():
-    response = requests.post(wekbook_url, data=json.dumps(
-        data), headers={'Content-Type': 'application/json'})
-
-    if (response.status_code != 200):
-        print('Response: ' + str(response.text))
-        print('Response code: ' + str(response.status_code))
-
+    webhook_url = 'Your-Slack-Webhook-URL'
     
+    send_data = "Good Morning Vineet!! :smile:\nToday it's " + days_list[day] + "\n\nTimetable is :"
+    
+    def webhook_send():
+        response = requests.post(webhook_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+    
+    
+    if (day == 0):
+        for i in range(len(Monday)):
+            send_data = send_data + " " + Monday[i]
 
-if (day == 0):
-    for i in range(5):
-        send_data = send_data + " " + Monday[i]
-        
-    data = {
-        'text': send_data,
-        'username': 'My Home',
-    }
+        data = { 'text': send_data }
+
+
+    if (day == 1):
+        for i in range(len(Tuesday)):
+            send_data = send_data + " " + Tuesday[i]
+    
+        data = { 'text': send_data }
+            
+    
+    if (day == 2):
+        for i in range(len(Wednesday)):
+            send_data = send_data + " " + Wednesday[i]
+    
+        data = { 'text': send_data }
+    
+    
+    if (day == 3):
+        for i in range(len(Thursday)):
+            send_data = send_data + " " + Thursday[i]
+    
+        data = { 'text': send_data }
+    
+    
+    if (day == 4):
+        for i in range(len(Friday)):
+            send_data = send_data + " " + Friday[i]
+    
+        data = { 'text': send_data }
+    
+    
+    if (day == 5):
+        for i in range(len(Saturday)):
+            send_data = send_data + " " + Saturday[i]
+
+        data = { 'text': send_data }
+    
+    
+    if (day == 6):
+        data = { 'text': 'Today is Sunday :smile: \nNo classes today. Enjoy!!!!' }
+            
+    
     webhook_send()
-
-
-if (day == 1):
-    for i in range(5):
-        send_data = send_data + " " + Tuesday[i]
-
-    data = {
-        'text': send_data,
-        'username': 'My Home',
-    }
-    webhook_send()
-
-if (day == 2):
-    for i in range(4):
-        send_data = send_data + " " + Wednesday[i]
-
-    data = {
-        'text': send_data,
-        'username': 'My Home',
-    }
-    webhook_send()
-
-
-if (day == 3):
-    for i in range(4):
-        send_data = send_data + " " + Thursday[i]
-
-    data = {
-        'text': send_data,
-        'username': 'My Home',
-    }
-    webhook_send()
-
-if (day == 4):
-    for i in range(4):
-        send_data = send_data + " " + Friday[i]
-
-    data = {
-        'text': send_data,
-        'username': 'My Home',
-    }
-    webhook_send()
-
-if (day == 5):
-    for i in range(4):
-        send_data = send_data + " " + Saturday[i]
-
-    data = {
-        'text': send_data,
-        'username': 'My Home',
-    }
-    webhook_send()
-
-if (day == 6):
-    data = {
-        'text': 'No classes today. Enjoy!!!!',
-        'username': 'My Home',
-    }
-    webhook_send()
+  
